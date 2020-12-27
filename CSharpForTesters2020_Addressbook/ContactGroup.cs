74c2c853fc9787688539068a3878153558b0ef57
oxy <76120633+OxyCSh@users.noreply.github.com>
2020-12-27 16:38:09 +0000

    Methods extracted from GroupCreationTests, GroupCreationTests and new methods modified to use two new classes (User and ContactGroup)

---
+++ CSharpForTesters2020_Addressbook/ContactGroup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressbookWebTests
+{
+    class ContactGroup
+    {
+        //private string name;
+        //private string header = "";
+        //private string footer = "";
+
+        public ContactGroup(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public string Header { get; set; } = "";
+        public string Footer { get; set; } = "";
+    }
+}
--- CSharpForTesters2020_Addressbook/GroupCreationTests.cs
+++ CSharpForTesters2020_Addressbook/GroupCreationTests.cs
@@ -66,24 +66,70 @@ namespace AddressbookWebTests
         [Test]
         public void GroupCreationTest()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("Yellow Group");
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("Yellow header");
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("Yellow footer");
-            driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("group page")).Click();
+            OpenHomePage();
+            Login(new User("admin", "secret"));
+            GoToGroupsPage();
+
+            InitGroupCreation();
+            ContactGroup group = new ContactGroup("Blue group");
+            group.Header = "Blue header";
+            group.Footer = "Blue footer";
+            FillGroupForm(group);
+            SubmitNewGroup();
+            ReturnToGroups();
+
+            Logout();
+        }
+
+        private void Logout()
+        {
             driver.FindElement(By.LinkText("Logout")).Click();
         }
+
+        private void ReturnToGroups()
+        {
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        private void SubmitNewGroup()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(ContactGroup group)
+        {
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+
+        private void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(User user)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(user.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(user.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
         private bool IsElementPresent(By by)
         {
             try
